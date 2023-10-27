83988243901116c6ef3059583b689ebdd50cf552
Grzesiek <gzaber@tlen.pl>
2023-10-27 14:23:32 +0200

    create information_data_source package

---
+++ packages/information_data_source/example/information_data_source_example.dart
@@ -0,0 +1,6 @@
+import 'package:information_data_source/information_data_source.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/information_data_source/test/information_data_source_test.dart
@@ -0,0 +1,16 @@
+import 'package:information_data_source/information_data_source.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
