91735f1605702224ecbd62fd02fb6ec928a37f69
Grzesiek <gzaber@tlen.pl>
2023-10-28 14:57:32 +0200

    refactor create and update methods to save method

--- packages/information_data_source/lib/src/information_data_source.dart
+++ packages/information_data_source/lib/src/information_data_source.dart
@@ -1,8 +1,7 @@
 import 'model/model.dart';
 
 abstract interface class InformationDataSource {
-  Future<void> createInformation(Information information);
-  Future<void> updateInformation(Information information);
+  Future<void> saveInformation(Information information);
   Future<void> deleteInformation(int id);
   Stream<List<Information>> readAllInformation();
 }
