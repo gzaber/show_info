250df7eb498ab6815ad751673a3556fad6029f99
Grzesiek <gzaber@tlen.pl>
2023-11-14 14:50:55 +0100

    refactor objectbox data source to receive boxes in constructor

--- packages/objectbox_information_data_source/lib/src/objectbox_information_data_source.dart
+++ packages/objectbox_information_data_source/lib/src/objectbox_information_data_source.dart
@@ -4,38 +4,40 @@ import 'package:objectbox/objectbox.dart';
 import 'entities/entities.dart';
 
 class ObjectboxInformationDataSource implements InformationDataSource {
-  ObjectboxInformationDataSource({required Store store}) : _store = store;
+  ObjectboxInformationDataSource({
+    required Store store,
+    Box<InformationEntity>? informationBox,
+    Box<TextEntity>? textBox,
+  })  : _informationBox = informationBox ?? store.box<InformationEntity>(),
+        _textBox = textBox ?? store.box<TextEntity>();
 
-  final Store _store;
+  final Box<InformationEntity> _informationBox;
+  final Box<TextEntity> _textBox;
 
   @override
   Future<void> saveInformation(Information information) async {
-    await _store
-        .box<InformationEntity>()
-        .putAsync(InformationEntity.fromModel(information));
+    await _informationBox.putAsync(InformationEntity.fromModel(information));
   }
 
   @override
   Future<void> deleteInformation(int id) async {
-    await _store.box<InformationEntity>().removeAsync(id);
+    await _informationBox.removeAsync(id);
   }
 
   @override
   Future<void> saveManyTexts(List<Text> texts) async {
-    await _store
-        .box<TextEntity>()
+    await _textBox
         .putManyAsync(texts.map((t) => TextEntity.fromModel(t)).toList());
   }
 
   @override
   Future<void> deleteManyTexts(List<int> ids) async {
-    await _store.box<TextEntity>().removeManyAsync(ids);
+    await _textBox.removeManyAsync(ids);
   }
 
   @override
   Stream<List<Information>> readAllInformation() {
-    return _store
-        .box<InformationEntity>()
+    return _informationBox
         .query()
         .watch(triggerImmediately: true)
         .map((query) => query.find().map((info) => info.toModel()).toList());
